7c782b4cb634f3b9540d761bc18759f7b2cd33dc
Arnau González Acosta <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-09-26 10:02:15 +0200

    Scene NavMesh

---
+++ Assets/movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movement : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
