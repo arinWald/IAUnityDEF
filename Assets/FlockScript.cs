3b757eb3e3e42124b980f0a81fca6208ff6f0d97
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-10-05 14:46:28 +0200

    first commit

--- Assets/FlockScript.cs
+++ Assets/FlockScript.cs
@@ -1,66 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class FlockScript : MonoBehaviour
+public class Flocking : MonoBehaviour
 {
-    public FlockingManager myManager;
-    private float speed;
-    public Vector3 direction;
-    float freq = 0f;
-    float time = 1.0f;
-    // Start is called before the first frame update
+    public float speed;
+    bool turning = false;
+
     void Start()
     {
-        
+        speed = Random.Range(FlockingManager.myManager.minSpeed, FlockingManager.myManager.maxSpeed);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        freq += Time.deltaTime;
-        if (freq > time)
+        Bounds b = new Bounds(FlockingManager.myManager.transform.position, FlockingManager.myManager.swimLimits * 2.0f);
+
+        if (!b.Contains(transform.position))
         {
-            freq -= time;
-            NewMethod();
+
+            turning = true;
+        }
+        else
+        {
+
+            turning = false;
         }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation,
-                                      Quaternion.LookRotation(direction),
-                                      myManager.rotationSpeed * Time.deltaTime);
+        if (turning)
+        {
+
+            Vector3 direction = FlockingManager.myManager.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(direction),
+               FlockingManager.myManager.rotationSpeed * Time.deltaTime);
+
+        }
+        else
+        {
+
+            if (Random.Range(0, 100) < 10)
+            {
+
+                speed = Random.Range(FlockingManager.myManager.minSpeed, FlockingManager.myManager.maxSpeed);
+            }
+
+
+            if (Random.Range(0, 100) < 10)
+            {
+                ApplyRules();
+            }
+        }
         transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
-
-    private void NewMethod()
+    private void ApplyRules()
     {
+
+        GameObject[] allFish;
+        allFish = FlockingManager.myManager.allFish;
+
         Vector3 cohesion = Vector3.zero;
         Vector3 align = Vector3.zero;
         Vector3 separation = Vector3.zero;
         int num = 0;
 
-        foreach (GameObject go in myManager.allFish)
+
+        foreach (GameObject go in allFish)
         {
             if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
-                if (distance <= myManager.neighbourDistance)
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+
+                if (distance <= FlockingManager.myManager.neighbourDistance)
                 {
                     cohesion += go.transform.position;
-                    align += go.GetComponent<FlockScript>().direction;
-                    separation -= (transform.position - go.transform.position) /
-                         (distance * distance);
                     num++;
+                    align += (this.transform.position - go.transform.position);
+                    num++;
+                    separation -= (transform.position - go.transform.position) / (distance * distance);
+
                 }
             }
         }
+
         if (num > 0)
         {
+
             cohesion = (cohesion / num - transform.position).normalized * speed;
             align /= num;
-            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
-        }
+            speed = Mathf.Clamp(align.magnitude, FlockingManager.myManager.minSpeed, FlockingManager.myManager.maxSpeed);
 
-        direction = (cohesion * myManager.cohesionLevel + align * myManager.alignLevel + separation * myManager.separationLevel).normalized * speed;
+
+            Vector3 direction = (cohesion + align) - transform.position;
+            if (direction != Vector3.zero)
+            {
+
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    Quaternion.LookRotation(direction),
+                   FlockingManager.myManager.rotationSpeed * Time.deltaTime);
+            }
+
+        }
     }
+
+
 }
+
--- Assets/FlockingManager.cs
+++ Assets/FlockingManager.cs
@@ -1,32 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FlockingManager : MonoBehaviour
 {
+    public static FlockingManager myManager;
+
+
     public GameObject fishPrefab;
     public int numFish;
-    public Vector3 swimLimits;
-    public bool bounded;
-    public bool randomize;
-    public bool followLider;
-    public GameObject lider;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5.0f, 5.0f, 5.0f); public GameObject Lider;
 
-    [Header("Fish Settings")]
-    [Range(0.0f, 10.0f)]
+    [Range(1.0f, 10.0f)]
     public float minSpeed;
-    [Range(0.0f, 10.0f)]
+    [Range(1.0f, 10.0f)]
     public float maxSpeed;
-    [Range(0.0f, 10.0f)]
+    [Range(1.0f, 10.0f)]
     public float neighbourDistance;
-    [Range(0.0f, 10.0f)]
+    [Range(1.0f, 10.0f)]
     public float rotationSpeed;
 
-    public GameObject[] allFish;
 
-    public float cohesionLevel;
-    public float alignLevel;
-    public float separationLevel;
+    //[Range(-1f, 1f)]
+    //public float COH_W;
+    //[Range(-1f, 1f)]
+    //public float AL_W;
+    //[Range(-1f, 1f)]
+    //public float SEP_W;
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,16 +38,46 @@ public class FlockingManager : MonoBehaviour
         allFish = new GameObject[numFish];
         for (int i = 0; i < numFish; ++i)
         {
-            Vector3 pos = this.transform.position + new Vector3(Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f)); // random position
-            Vector3 randomize = new Vector3(Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f), Random.Range(0.0f, 10.0f)).normalized;// random vector direction
+            Vector3 pos = this.transform.position + new Vector3(
+               Random.Range(-swimLimits.x, swimLimits.x),
+               Random.Range(-swimLimits.y, swimLimits.y),
+               Random.Range(-swimLimits.z, swimLimits.z));
+            Vector3 randomize = Random.onUnitSphere;  // random vector direction normalized
+
             allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.LookRotation(randomize));
-            allFish[i].GetComponent<FlockScript>().myManager = this;  
+            myManager = this;
         }
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
 
+        //swimlimits
+
+        for (int i = 0; i < numFish; ++i)
+        {
+            Vector3 pos = allFish[i].transform.position;
+
+            if (pos.x < -swimLimits.x || pos.x > swimLimits.x)
+            {
+                pos.x = Mathf.Clamp(pos.x, -swimLimits.x, swimLimits.x);
+            }
+
+            if (pos.y < -swimLimits.y || pos.y > swimLimits.y)
+            {
+                pos.y = Mathf.Clamp(pos.y, -swimLimits.y, swimLimits.y);
+            }
+
+            if (pos.z < -swimLimits.z || pos.z > swimLimits.z)
+            {
+                pos.z = Mathf.Clamp(pos.z, -swimLimits.z, swimLimits.z);
+            }
+
+            allFish[i].transform.position = pos;
+        }
     }
+
 }
