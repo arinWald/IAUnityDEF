b21a8d39d9409020fc50eddce7d66cca8dbe85d1
Arnau González Acosta <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-10-11 11:58:18 +0200

    use NavMeshAgent

--- Assets/FollowAnObject.cs
+++ Assets/FollowAnObject.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using static UnityEditor.FilePathAttribute;
 using static UnityEngine.GraphicsBuffer;
 
 public class FollowAnObject : MonoBehaviour
 {
     public GameObject target;
+    public NavMeshAgent agent;
     public float maxVelocity;
     [SerializeField]
     private float velocity;
@@ -15,9 +17,12 @@ public class FollowAnObject : MonoBehaviour
     public float turnSpeed;
     float freq = 0f;
 
+    public bool useWander;
+
     // Start is called before the first frame update
     void Start()
     {
+        useWander = true;
         transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(10, -10) / 2);
         transform.rotation = Quaternion.EulerRotation(0, Random.Range(0, 360), 0);
 
@@ -30,20 +35,28 @@ public class FollowAnObject : MonoBehaviour
 
     private void Seek()
     {
-        // Seek
-        Vector3 direction = target.transform.position - transform.position;
-        direction.y = 0f;    // (x, z): position in the floor
+        if(useWander)
+        {
+            agent.destination = target.transform.position;
+        }
+        else
+        {
+            // Seek
+            Vector3 direction = target.transform.position - transform.position;
+            direction.y = 0f;    // (x, z): position in the floor
 
-        Vector3 movement = direction.normalized * maxVelocity;
+            Vector3 movement = direction.normalized * maxVelocity;
 
-        float angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);  // up = y
+            float angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);  // up = y
 
-        Vector3.Distance(target.transform.position, transform.position);
-        angle = Mathf.Abs(Vector3.Angle(transform.forward, movement));  // forward = z
+            Vector3.Distance(target.transform.position, transform.position);
+            angle = Mathf.Abs(Vector3.Angle(transform.forward, movement));  // forward = z
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation,
-                                      Time.deltaTime * turnSpeed);
-        transform.position += transform.forward.normalized * maxVelocity * Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation,
+                                          Time.deltaTime * turnSpeed);
+            transform.position += transform.forward.normalized * maxVelocity * Time.deltaTime;
+        }
+        
     }
 }
