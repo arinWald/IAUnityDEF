9464af64e7e799a928656c3c26c4dbf6116b8be6
Arnau González Acosta <arnau.gonzalez.acosta@estudiantat.upc.edu>
2023-10-17 09:56:16 +0200

    alsdjlashd

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -6,12 +6,14 @@ public class PlayerControl : MonoBehaviour
 {
 
     private Patrol patrolScript;
+    private CapsuleCollider collider;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("asd");
         patrolScript = GetComponent<Patrol>();
+        collider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -28,6 +30,7 @@ public class PlayerControl : MonoBehaviour
             patrolScript.stopPatrolling = true;
             patrolScript.agent.speed = 0;
             Debug.Log("Trigger Triggered");
+            this.gameObject.SetActive(false);
         }
     }
 }
